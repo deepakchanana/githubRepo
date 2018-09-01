64e7368e9f872fe48731b1ece5004ea3d3e4bcd6
Dell <Dell@DESKTOP-30FNJH5>
2018-08-31 20:00:06 -0700

    Want to push the code

---
+++ project2/src/pkg1/class1.groovy
@@ -0,0 +1,15 @@
+package pkg1
+
+class class1 {
+	
+	public static void main(String[] args)
+	{
+		HashSet<Integer> hs=new HashSet<Integer>();
+		
+		hs.add(12);
+		hs.add(13);
+		
+		println hs.getAt(0)
+    }
+	}
+
